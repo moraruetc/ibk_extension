14fa9930c09a13ce3d835df230fea4488bcca3c6
moraruetc <moraru.etc@gmail.com>
2013-11-19 19:07:09 +0200

    parse deposits only

--- app/portfolio.js
+++ app/portfolio.js
@@ -8,25 +8,36 @@ var moro_PORTFOLIO = function(){
 	}
 
 	function parseCurrentAccounts(){
+
 		var amountEUR = 0;
 		var amountRON = 0;
+
+		clientData.deposits = [];
+		clientData.totalDeposits = {};
   
-		$('span.current.total').each(function(index, data){
+		$('li.time span.current.total').each(function(index, data){
 			var text = $(this).text().trim();
 			text = text.replace('\.', '');
 			text = text.replace('\,', '.');
-			if(text.indexOf('RON') == 0){
-				amountRON += parseFloat(text.replace('RON', ''));
-			}else if(text.indexOf('EUR') == 0){
-				amountEUR += parseFloat(text.replace('EUR', ''));				
-			}		
+
+			var currency = text.substr(0, 3);
+
+			var amount = parseFloat(text.replace(currency, ''));
+			clientData.deposits.push({currency:currency, amount: amount});
+			
+			clientData.totalDeposits[currency] = 0;
+		});
+
+		$.each(clientData.deposits, function(i, dep){
+			var n = clientData.totalDeposits[dep.currency]; 
+			n = n + dep.amount;
+			clientData.totalDeposits[dep.currency] = n;
 		});
 	
-		amountEUR = amountEUR.toFixed(2);
-		amountRON = amountRON.toFixed(2);		
+		//amountEUR = amountEUR.toFixed(2);
+		//amountRON = amountRON.toFixed(2);		
 
-		clientData.amountEUR = amountEUR;
-		clientData.amountRON = amountRON;
+		console.log(clientData);		
   		
 	}
 
@@ -38,7 +49,7 @@ var moro_PORTFOLIO = function(){
 		$.when($.get(chrome.extension.getURL('templates/portfolio_section.tp')), $.get(chrome.extension.getURL('templates/portfolio_section_element.tp')))
 		.done(function(section, element){
 			$("#portfolio-accounts").append(section[0]);			
-			$("#portfolio-accounts").append( _.template(element[0], clientData));
+			//$("#portfolio-accounts").append( _.template(element[0], clientData));
 		});
 	}
 
@@ -46,7 +57,7 @@ var moro_PORTFOLIO = function(){
 		init:function(){
 			parseCurrencies();
 			parseCurrentAccounts();
-			parseCurrentAccounts();
+			parseDeposits();
 		}, 
 
 		addTotals:function(){
