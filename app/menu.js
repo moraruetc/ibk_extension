95ca13facb7fb4385eee2f7a9801fc2bfde93d84
moraruetc <moraru.etc@gmail.com>
2013-11-20 14:18:17 +0200

    better menu async

--- app/menu.js
+++ app/menu.js
@@ -3,29 +3,35 @@ var moro_MENU = function(){
 	function addMenuOption(){
 		var menu = '';
 
-		$.get(chrome.extension.getURL('templates/menu_element.tp'), function(template) {
-     		option = _.template(template);
+
+		var promise = $.get(chrome.extension.getURL('templates/menu_element.tp'));
+
+		promise.done(function(template) {
+     		var option = _.template(template);
     		$('#menu').append(option);
-
     		$('#moro_get_more').click(function(){
-
-
 			$("#menu").find("ul").not(".active ul").not($(this).find("ul")).slideUp();
-			
-		
+    		$('#menu .active').removeClass('active');
+    		$('#moro_get_more').addClass('active');
+    		$('#main').html('');
+    		$.get(chrome.extension.getURL('templates/get_more.tp'), function(template) {
 
-    			$('#menu .active').removeClass('active');
-    			$('#moro_get_more').addClass('active');
+    			$('#main').html(template);
+
+				buildSampleChart();
 
 
-    			$('#main').html('');
-    			$.get(chrome.extension.getURL('templates/get_more.tp'), function(template) {
-    				$('#main').html(template);
+   				}, 'html');
+    		});
 
-var data = [
-    ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
-    ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
-  ];
+		});	
+	}
+
+	function buildSampleChart(){
+		var data = [
+  				  ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
+    				['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+ 				 ];
   var plot1 = $.jqplot ('moro_chart1', [data],
     {
       seriesDefaults: {
@@ -40,14 +46,6 @@ var data = [
       legend: { show:true, location: 'e' }
     }
   );
-
-
-   				}, 'html');
-    		});
-
-		}, 'html');
-
-
 	}
 	
 	return {
